3dfe6f5bc53cc73c282544ce78a3f89d759323a7
Ефремов Николай <fa_efremov@mail.ru>
2024-09-10 21:54:49 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,10 @@
+for n in range(3,21):
+    pairs = []
+    for i in range(1,21):
+        for j in range(1+i,21):
+            if n % (i + j) == 0:
+                pairs.append(f'{i}{j}')
+            else:
+                continue
+    result = ''.join(pairs)
+    print(f'{n} - {result}')
